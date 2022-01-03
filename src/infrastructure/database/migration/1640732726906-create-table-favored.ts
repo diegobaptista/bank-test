ae0d961daf569f473f0f2df83e61208972c6039e
Diego Baptista <diego.baptista@tegra.com.br>
2022-01-03 20:19:43 -0300

    try fix migration

--- src/infrastructure/database/migration/1640732726906-create-table-favored.ts
+++ src/infrastructure/database/migration/1640732726906-create-table-favored.ts
@@ -1,16 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createTableFavored1640732726906 implements MigrationInterface {
-    name = 'createTableFavored1640732726906'
+  name = "createTableFavored1640732726906";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "favored" ADD "email" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "favored" ADD "status" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "favored" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "status" character varying NOT NULL, "documentType" character varying NOT NULL, "document" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "PK_60d4711d5f763ccef8016c423c0" PRIMARY KEY ("id"))`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "favored" DROP COLUMN "status"`);
-        await queryRunner.query(`ALTER TABLE "favored" DROP COLUMN "email"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "favored"`);
+  }
 }
